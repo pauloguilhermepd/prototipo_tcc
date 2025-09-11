07cc6edfed9f0f44e22c2b4f0b17280edd56529c
pauloguilhermepd <pauloguilhermepdomingos@gmail.com>
2025-09-11 20:27:53 -0300

    Fiz o login, agora ele está fazendo a verificação e da pra acessar o aplicativo. Agora fiz a tela principal para mostrar se o usuário conseguiu cadastrar ou logar.

--- app/src/main/java/com/example/teste1/view/formcadastro/FormCadastro.java
+++ app/src/main/java/com/example/teste1/view/formcadastro/FormCadastro.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.teste1.R;
 import com.example.teste1.databinding.ActivityFormCadastroBinding;
 import com.example.teste1.view.formlogin.FormLogin;
+import com.example.teste1.view.telaprincipal.TelaPrincipal;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,7 +19,6 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class FormCadastro extends AppCompatActivity {
-    //Criando a variavel binding para usar no codigo
     private ActivityFormCadastroBinding binding;
     private FirebaseAuth auth  = FirebaseAuth.getInstance();
     @Override
@@ -29,59 +29,47 @@ public class FormCadastro extends AppCompatActivity {
         binding = ActivityFormCadastroBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //Se clicar no texto de não ter conta ele vai pra tela de login
         binding.txtLogin.setOnClickListener(view -> {
             Intent intent = new Intent(FormCadastro.this, FormLogin.class);
             startActivity(intent);
             finish();
         });
 
-        //Clicar no botão de cadastrar
         binding.btnCadastrar.setOnClickListener(view -> {
-            String email = binding.editEmail.getText().toString();
-            String senha = binding.editSenha.getText().toString();
+            String email = binding.editEmailCadastro.getText().toString();
+            String senha = binding.editSenhaCadastro.getText().toString();
 
-            //Se verificar se o email esta vazio
             if(email.isEmpty() || senha.isEmpty()){
                 Snackbar snackbar = Snackbar.make(view, "Preencha todos os campos", Snackbar.LENGTH_SHORT);
                 snackbar.setBackgroundTint(Color.RED);
                 snackbar.show();
-            //Se não estiver vazio:
             }else{
-                //Cadastra o usuário utilizando o firebase
                 auth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(cadastro -> {
-                    //Verifica se o cadastro foi realizado com sucesso atraves do lambda
                     if(cadastro.isSuccessful()){
 
-                        //Criar um snackbar com uma mensagem de sucesso
-                        Snackbar snackbar = Snackbar.make(view, "Cadastro realizado com sucesso", Snackbar.LENGTH_SHORT);
-                        snackbar.setBackgroundTint(Color.BLUE);
-                        snackbar.show();
-                        binding.editEmail.setText("");
-                        binding.editSenha.setText("");
-
-
+                        Intent intent = new Intent(FormCadastro.this, TelaPrincipal.class);
+                        startActivity(intent);
+                        finish();
                     }
-                    //Se o cadastro não for realizado ele cria uma exception para verificar o erro
+
                 }).addOnFailureListener(e -> {
                     String msgErro = "Erro ao cadastrar usuário";
-                    //Erro de senha inválida
+
                     if (e instanceof FirebaseAuthWeakPasswordException) {
                         msgErro = "Digite uma senha com no mínimo 6 caracteres";
-                    //Erro de email inválido
                     } else if (e instanceof FirebaseAuthInvalidCredentialsException) {
                         msgErro = "Digite um email válido";
-                    //Erro de email ja cadastrado
                     } else if (e instanceof FirebaseAuthUserCollisionException) {
                         msgErro = "Esta conta já foi cadastrada";
-                    //Erro de conexão
                     } else if(e instanceof FirebaseNetworkException){
                         msgErro = "Sem acesso a internet";
                     }
-                    //Cria um snack bar para mostrar a mensagem de erro
                     Snackbar snackbar = Snackbar.make(view, msgErro, Snackbar.LENGTH_SHORT);
                     snackbar.setBackgroundTint(Color.RED);
                     snackbar.show();
+
+                    binding.editEmailCadastro.setText("");
+                    binding.editSenhaCadastro.setText("");
 
                 });
             }
--- app/src/main/java/com/example/teste1/view/formlogin/FormLogin.java
+++ app/src/main/java/com/example/teste1/view/formlogin/FormLogin.java
@@ -1,5 +1,7 @@
 package com.example.teste1.view.formlogin;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,20 +12,63 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.teste1.R;
 import com.example.teste1.databinding.ActivityFormLoginBinding;
+import com.example.teste1.view.formcadastro.FormCadastro;
+import com.example.teste1.view.telaprincipal.TelaPrincipal;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class FormLogin extends AppCompatActivity {
     private ActivityFormLoginBinding binding;
-
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_form_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityFormLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.txtLinkCadastrar.setOnClickListener(view -> {
+            Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+            startActivity(intent);
+            finish();
         });
+
+        binding.btnEntrar.setOnClickListener(view -> {
+            String email = binding.editEmailLogin.getText().toString();
+            String senha = binding.editSenhaLogin.getText().toString();
+
+            if(email.isEmpty() || senha.isEmpty()){
+                Snackbar snackbar = Snackbar.make(view, "Preencha todos os campos", Snackbar.LENGTH_SHORT);
+                snackbar.setBackgroundTint(Color.RED);
+                snackbar.show();
+            }else{
+                auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(login -> {
+                    if(login.isSuccessful()){
+                        Intent intent = new Intent(FormLogin.this, TelaPrincipal.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }).addOnFailureListener(e -> {
+                    String msgErro = "Erro ao fazer login";
+                    if(e instanceof FirebaseAuthInvalidCredentialsException){
+                        msgErro = "Email ou senha incorretos";
+                    } else if(e instanceof FirebaseNetworkException){
+                        msgErro = "Sem conexão com a internet";
+                    } else if(e instanceof FirebaseAuthInvalidUserException){
+                        msgErro = "Usuário não cadastrado";
+                    }
+                    Snackbar snackbar = Snackbar.make (view, msgErro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.RED);
+                    snackbar.show();
+                });
+            }
+        });
+
     }
 }
